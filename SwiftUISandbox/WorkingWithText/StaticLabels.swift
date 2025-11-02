85f8abb5ffdd331c1148debe8e9188b8ec3550a6
Raphael <rewoma19@gmail.com>
2025-11-02 13:23:03 -0700

    Create and style Text views

--- SwiftUISandbox/WorkingWithText/StaticLabels.swift
+++ SwiftUISandbox/WorkingWithText/StaticLabels.swift
@@ -1,15 +1,32 @@
-//
-//  StaticLabels.swift
-//  SwiftUISandbox
-//
-//  Created by Raphael Odeareduo on 2025-11-01.
-//
-
 import SwiftUI
 
 struct StaticLabels: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+//            By default, Text views in SwiftUI are centered in the main / parent view
+    //  By default, text views wrap across as many lines as possible, but you can limit the number of lines shown, by using the lineLimit modifier, which takes an Integer argument
+            Text("This is some longer text that is limited to three lines maximum, so anything more than that will cause the text to clip.")
+                .lineLimit(2)
+                .frame(width: 200)
+            
+//            The text view directly below will allow its content to be within 2 to 4 lines. This means that if it should grow up to 5 lines or more, the overflowing text would be shown as ellipsis (...)
+            
+            Text("This is some longer text that is limited to a specific range of lines, so anything more than four lines will cause the text to clip.")
+                .lineLimit(2...4)
+                .frame(width: 200)
+            
+//            Using the reservesSpace parameter with the lineLimit modifier on this Text view below, means that it will be sized to be exactly two lines in height.
+//            Even if the text is short and only needs a single line, the content itself won't be stretched but the size of the view will be two lines in height
+            Text("This is always two lines")
+                .lineLimit(2, reservesSpace: true)
+            
+//            When using the lineLimit modifier on a Text view, by default, it truncates the text from the end and places an ellipsis at the end.
+//            You cna change where you want to truncate the text and show the ellipsis, using another modifier alongside the lineLimit one, called truncationMode, which could take arguments: .head, .middle, .tail (which is the default)
+            Text("This is an extremely long string of text that will never fit even the widest of iOS devices even if the user has their Dynamic Type setting as small as is possible, so in theory it should definitely demonstrate truncationMode().")
+                .lineLimit(1)
+                .truncationMode(.middle)
+        }
+        .padding()
     }
 }
 
--- SwiftUISandbox/WorkingWithText/StylingText.swift
+++ SwiftUISandbox/WorkingWithText/StylingText.swift
@@ -1,15 +1,39 @@
-//
-//  StylingText.swift
-//  SwiftUISandbox
-//
-//  Created by Raphael Odeareduo on 2025-11-01.
-//
-
 import SwiftUI
 
 struct StylingText: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            //  You can modify the font size of your Text using the font modifier.
+            //  You can also modify the size of the text VIEW using the frame modifier, which takes width, height, and more arguments
+//            You can increase / decrease the line spacing of your text by using the lineSpacing modifier with takes Int arguments. The default is 0
+            Text("This is an extremely long text string that will never fit even the widest of phones without wrapping")
+                .font(.largeTitle)
+                .lineSpacing(20)
+                .frame(width: 300)
+            
+//            You can change the color of your Text by using the foregroundStyle modifer, which can take arguments .red, .blue, and more
+            Text("The best laid plans")
+                .foregroundStyle(.red)
+            
+//            You could even use colored gradients on your text
+            Text("The second best laid plans")
+                .foregroundStyle(.yellow.gradient)
+            
+//            You can also apply a background color to a Text view by using the background modifier
+            Text("The 3rd best laid plans")
+                .padding()
+                .background(.blue)
+                .foregroundStyle(.white)
+                .font(.headline)
+            
+//            We can change the font style by using the fontDesign modifier. Some of its arguments are .serif, .monospaced, etc.
+            Text("Hello, world!")
+                .fontDesign(.monospaced)
+            
+//            We could compress or expant the font of a Text view using the fontWidth modifier. Note that is only works if the font has been designed to support it.
+            Text("Hello, world!")
+                .fontWidth(.condensed)
+        }
     }
 }
 
